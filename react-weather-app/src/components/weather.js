bfb1687adb712340b3daf8c1eb9d1f0f5c04350f
Sumit <sumit.das@publicissapient.com>
2022-03-22 10:25:27 +0530

    Added Weather Icons to App

--- react-weather-app/src/components/weather.js
+++ react-weather-app/src/components/weather.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import WeatherDetails from './weatherDetails'
 
 const Weather = () => {
     return (
         <>  
-            <div class="weather-wrapper"> 
-                <div class="heading items-center items-center justify-center grid grid-rows-1 grid-flow-col gap-4 py-5">
-                    <h2 class='text-lg'> 5 Day Forecast </h2>
-                </div>           
+            <div className="weather-wrapper"> 
+                <div className="heading items-center items-center justify-center grid grid-rows-1 grid-flow-col gap-4 py-5">
+                    <h2 className='text-lg'> 5 Day Forecast </h2>
+                </div> 
+                <WeatherDetails />          
             </div>
         </>
     );

dcf5d01f906b9b52b55496a21a884e5955dca290
Sumit <sumit.das@publicissapient.com>
2022-03-24 09:36:29 +0530

    Used OpenWeather API to fetch response

--- react-weather-app/src/components/weatherDetails.js
+++ react-weather-app/src/components/weatherDetails.js
@@ -1,3 +1,6 @@
+import axios from 'axios' 
+import {useEffect, useState} from 'react'
+
 const products = [
     {
       id: 1,
@@ -42,11 +45,29 @@ const products = [
   ]
   
   export default function Example() {
+
+    const [cityName, setCityName] = useState();
+    //const [weatherConditions, setweatherConditions] = useState();
+
+    useEffect(()=>{
+        axios.get('https://api.openweathermap.org/data/2.5/forecast?lat=35&lon=139&appid=7fa8ec92190927efb89d316589df0a71')
+            .then((response)=>{
+                setCityName(`${response.data.city.name},${response.data.city.country}` )
+                // let weatherConditions = response.data.map((weather)=>{
+                //     return (
+                //         <h3 className="mt-4 text-sm text-gray-700"></h3>
+                //     )
+                // })
+                console.log(response.data)
+            
+            })
+    })
+
     return (
       <div className="bg-white">
         <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-          <h2 className="sr-only">Products</h2>
-  
+          <h2 className="sr-only">Weather Conditions</h2>
+            <p>{cityName}</p>
           <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
             {products.map((product) => (
               <a key={product.id} href={product.href} className="group">
@@ -56,9 +77,7 @@ const products = [
                   <i className= {`wi ${product.imageSrc}`}></i>
                   <p className="mt-1 text-lg font-medium text-gray-900">{product.temp}</p>
                   <p className="mt-1 text-lg font-medium text-gray-900">{product.conditions}</p>
-                </div>
-                
-                
+                </div>                                
               </a>
             ))}
           </div>

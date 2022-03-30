4dfa8d315eeafcc21eea2bd50c59a0ed95204dbc
Sumit <sumit.das@publicissapient.com>
2022-03-30 23:47:16 +0530

    Added 5 days forecast

--- react-weather-app/src/components/weatherDetails.js
+++ react-weather-app/src/components/weatherDetails.js
@@ -13,19 +13,22 @@ export default function Example() {
   ];
   const [cityName, setCityName] = useState();
   const [weatherConditions, setweatherConditions] = useState();
-
+  let count =0;
   useEffect(() => {
     axios
       .get(
-        "https://api.openweathermap.org/data/2.5/forecast?lat=35&lon=139&appid=7fa8ec92190927efb89d316589df0a71&units=metric"
+        "https://api.openweathermap.org/data/2.5/onecall?lat=35&lon=139&exclude=hourly,minutely&appid=7fa8ec92190927efb89d316589df0a71&unit=metric"
       )
       .then((response) => {
-        setCityName(`${response.data.city.name},${response.data.city.country}`);
-        let weatherConditions = response.data.list.filter((i) => {
-          return (
-            new Date(i.dt * 1000).toLocaleTimeString() ===
-            new Date(1648144800 * 1000).toLocaleTimeString()
-          );
+        //setCityName(`${response.data.city.name},${response.data.city.country}`);
+        let weatherConditions = response.data.daily.filter((date) => {          
+          console.log(count);
+          if(count < 5){
+            count ++;
+            return (
+              new Date(date.dt * 1000).toLocaleTimeString()
+            );
+          }          
         });
         setweatherConditions(weatherConditions);
       });
@@ -34,7 +37,7 @@ export default function Example() {
   let showData = "";
   if (weatherConditions) {
     showData = weatherConditions.map((weather) => {
-      console.log(weather);
+      //console.log(weather);
       return (
         <a key={weather.dt} href="#" className="group">
           <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8 text-center">
@@ -42,15 +45,15 @@ export default function Example() {
               {day[new Date(weather.dt * 1000).getDay()]}
             </h3>
             <p className="mt-1 text-lg font-medium text-gray-900">
-              {weather.dt_txt}
+            {new Date(weather.dt * 1000).toLocaleDateString()}
             </p>
             <i className={`wi ${dict[weather.weather[0].icon]}`}></i>
             <p className="mt-1 text-lg font-medium text-gray-900">
-              {weather.main.temp}
+              {weather.temp.day}
             </p>
-            <p className="mt-1 text-lg font-medium text-gray-900">
+            {/* <p className="mt-1 text-lg font-medium text-gray-900">
               {weather.weather[0].description}
-            </p>
+            </p> */}
           </div>
         </a>
       );

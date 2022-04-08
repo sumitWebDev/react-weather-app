f64c6e3d8ce240a4a585748566dfd36b5965cb2e
Sumit <dassumit.ece@gmail.com>
2022-04-08 20:34:44 +0530

    Added moment.js library

--- react-weather-app/src/components/day.js
+++ react-weather-app/src/components/day.js
@@ -3,44 +3,33 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { FetchWeatherDayAsync } from "../actions/weather.actions";
 import dict from "../dict";
-import Moment from 'react-moment';
-import 'moment-timezone';
+import moment from "moment";
 
 const Day = (props) => {
     const dispatch = useDispatch();
     const { weatherEachDay } = useSelector((store) => store);;
-
-    let day = [
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday",
-        "Saturday",
-        "Sunday",
-      ];
     useEffect(() => {
         dispatch(FetchWeatherDayAsync());
     }, [dispatch])
     console.log(weatherEachDay)
     let dataEachDay = weatherEachDay.map((day) => {
         return (
-            <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8 text-center">
-            <h3 className="mt-4 text-sm text-gray-700">
-              {/* {day[new Date(day.dt * 1000).getDay()]}
-              {new Date(day.dt * 1000).getTime()} */}
-              <Moment parse="YYYY-MM-DD HH:mm">{new Date(day.dt*1000)}</Moment>
-            </h3>
-            <p className="mt-1 text-lg font-medium text-gray-900">
-              {/* {new Date(day.dt * 1000).toLocaleDateString()} */}
-            </p>
-            <i className={`wi ${dict[day.weather[0].icon]}`}></i>
-            <p className="mt-1 text-lg font-medium text-gray-900">
-              {day.main.temp}&deg;C
-            </p>
-            <p className="mt-1 text-lg font-medium text-gray-900">
-              {day.weather[0].description}
-            </p>
+            <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+                <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8 text-center">
+                    <h3 className="mt-4 text-sm text-gray-700">
+                    {moment(new Date(day.dt*1000)).format("hh:mm A")}
+                    </h3>
+                    <p className="mt-1 text-lg font-medium text-gray-900">
+                    {/* {new Date(day.dt * 1000).toLocaleDateString()} */}
+                    </p>
+                    <i className={`wi ${dict[day.weather[0].icon]}`}></i>
+                    <p className="mt-1 text-lg font-medium text-gray-900">
+                    {day.main.temp}&deg;C
+                    </p>
+                    <p className="mt-1 text-lg font-medium text-gray-900">
+                    {day.weather[0].description}
+                    </p>
+                </div>
           </div>
         )
     })
--- react-weather-app/src/components/weatherDetails.js
+++ react-weather-app/src/components/weatherDetails.js
@@ -3,19 +3,11 @@ import { useSelector, useDispatch } from "react-redux";
 import { FetchWeatherDetailsAsync } from "../actions/weather.actions";
 import { Link } from 'react-router-dom'
 import dict from "../dict";
+import moment from "moment";
 
 export default function WeatherDetails(props) {
 
   //variable declarations
-  let day = [
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-    "Sunday",
-  ];
   let dailyWeather = '';
   let weatherCount = 0;
 
@@ -37,10 +29,10 @@ export default function WeatherDetails(props) {
         <Link to='/day' key={weather.dt} target='blank' className="group">
           <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8 text-center">
             <h3 className="mt-4 text-sm text-gray-700">
-              {day[new Date(weather.dt * 1000).getDay()]}
+              {moment(new Date(weather.dt * 1000)).format("dddd ")}
             </h3>
             <p className="mt-1 text-lg font-medium text-gray-900">
-              {new Date(weather.dt * 1000).toLocaleDateString()}
+              {moment(new Date(weather.dt * 1000)).format('DD/MM/YYYY')}
             </p>
             <i className={`wi ${dict[weather.weather[0].icon]}`}></i>
             <p className="mt-1 text-lg font-medium text-gray-900">

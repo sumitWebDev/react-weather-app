8a12e07eb9ecbc2bffb72e46f33e6077d14d4ccc
Sumit <dassumit.ece@gmail.com>
2022-04-25 22:49:07 +0530

    Correct display of data

--- react-weather-app/src/components/weatherDetails.js
+++ react-weather-app/src/components/weatherDetails.js
@@ -22,20 +22,20 @@ export default function WeatherDetails(props) {
   }, [dispatch]);
   // getting duration of the day (night, day, evening, morning)
   const getDurationOfTheDay = (hour) => {
-    if (hour > 9 && hour <= 11)
+    if (hour > 6 && hour <= 11)
       return 'morn'
     if (hour > 11 && hour <= 17)
       return 'day'
     if (hour > 17 && hour <= 21)
       return 'eve'
-    if (hour > 4 && hour <= 9)
+    if (hour >= 22 || hour <= 5)
       return 'night'
   }
+  let durationOfDay = getDurationOfTheDay(parseInt(moment().format('H')));
   //Iterating daily weather objects
   dailyWeather = weatherDetailsList.map((weather) => {
     if (weatherCount < 5) {
       weatherCount++;
-      let durationOfDay = getDurationOfTheDay(moment(new Date(weather.dt * 1000)).format("H"));
       return (
         <Link to={`/${weather.dt}`} key={weather.dt} target='blank' className="group">
           <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8 text-center">
@@ -47,7 +47,7 @@ export default function WeatherDetails(props) {
             </p>
             <i className={`wi ${dict[weather.weather[0].icon]}`}></i>
             <p className="mt-1 text-lg font-medium text-gray-900">
-              {weather.temp[durationOfDay]}&deg;C
+              {weather.temp[durationOfDay]}{durationOfDay}&deg;C
             </p>
             <p className="mt-1 text-lg font-medium text-gray-900">
               {weather.weather[0].description}

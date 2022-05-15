9af3dce42e983d65a295ab9eacc145c89a4d508c
Sumit <dassumit.ece@gmail.com>
2022-05-15 12:09:05 +0530

    Styles for Day Details

--- react-weather-app/src/components/day.js
+++ react-weather-app/src/components/day.js
@@ -5,6 +5,7 @@ import { FetchWeatherDayAsync } from "../actions/weather.actions";
 import dict from "../dict";
 import moment from "moment";
 import { useParams } from 'react-router-dom';
+import '../styles/dayDetails.scss'
 
 const Day = (props) => {
     const dispatch = useDispatch();
@@ -18,14 +19,14 @@ const Day = (props) => {
             return (
 
                 <div key={day.dt} className="w-full text-center">
-                    <h3 className="mt-4 text-sm text-gray-700">
+                    <h3 className="text-white-700 time">
                         {moment(new Date(day.dt * 1000)).format("hh:mm A")}
                     </h3>
                     <i className={`wi ${dict[day.weather[0].icon]}`}></i>
-                    <p className="mt-1 text-lg font-medium text-gray-900">
+                    <p className="font-medium text-white-900">
                         {day.main.temp}&deg;C
                     </p>
-                    <p className="mt-1 text-lg font-medium text-gray-900">
+                    <p className="font-medium text-white-900">
                         {day.weather[0].description}
                     </p>
                 </div>
@@ -35,9 +36,8 @@ const Day = (props) => {
     })
     return (
         <>
-            Day to Day
-            <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8" >
-                <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+            <div className="day-details-cont">
+                <div className="">
                     {dataEachDay.length > 0 ? dataEachDay : '...Loading weather data'}
                 </div>
             </div>

7501ccc6486b7747cff5cae29becb950027c765a
Sumit <dassumit.ece@gmail.com>
2022-05-20 13:23:28 +0530

    Modified styles for Charts

--- react-weather-app/src/components/day.js
+++ react-weather-app/src/components/day.js
@@ -6,7 +6,7 @@ import dict from "../dict";
 import moment from "moment";
 import { useParams } from 'react-router-dom';
 import '../styles/dayDetails.scss'
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer,LabelList} from 'recharts';
+import { LineChart, Line, Legend, XAxis, ResponsiveContainer, LabelList } from 'recharts';
 
 
 const Day = (props) => {
@@ -85,35 +85,56 @@ const Day = (props) => {
     })
     return (
         <>
-            <div className="humidity-chart-cont">
-                <div className="humidity-chart">
-                    <h2>Humidity</h2>
-                <ResponsiveContainer width="100%" height="80%">
-                    <LineChart
-                        width={500}
-                        height={150}
-                        data={data}
-                        margin={{
-                            top: 10,
-                            right: 50,
-                            left: 50,
-                            bottom: 10,
-                        }}
-                    >
-                        {/* <Tooltip /> */}
-                        <XAxis dataKey="name" />
-                        <Legend />
-                        <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
-                        <LabelList dataKey="amountLabel" style={{ fill: "white" }} />
 
-                    </LineChart>
-                </ResponsiveContainer>
-
-                </div>
-                
-            </div>
             <div className="day-details-cont ">
-                
+                <div className="chart-cont grid grid-cols-4 gap-6">
+                    <div className="humidity-chart col-span-2">
+                        <h2>Humidity</h2>
+                        <ResponsiveContainer width="100%" height="80%">
+                            <LineChart
+                                width={500}
+                                height={150}
+                                data={data}
+                                margin={{
+                                    top: 10,
+                                    right: 50,
+                                    left: 50,
+                                    bottom: 10,
+                                }}
+                            >
+                                {/* <Tooltip /> */}
+                                <XAxis dataKey="name" />
+                                <Legend />
+                                <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
+                                <LabelList dataKey="amountLabel" style={{ fill: "white" }} />
+
+                            </LineChart>
+                        </ResponsiveContainer>
+                    </div>
+                    <div className="humidity-chart col-span-2">
+                        <h2>Humidity</h2>
+                        <ResponsiveContainer width="100%" height="80%">
+                            <LineChart
+                                width={500}
+                                height={150}
+                                data={data}
+                                margin={{
+                                    top: 10,
+                                    right: 50,
+                                    left: 50,
+                                    bottom: 10,
+                                }}
+                            >
+                                {/* <Tooltip /> */}
+                                <XAxis dataKey="name" />
+                                <Legend />
+                                <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
+                                <LabelList dataKey="amountLabel" style={{ fill: "white" }} />
+
+                            </LineChart>
+                        </ResponsiveContainer>
+                    </div>
+                </div>
                 <div className="day-details grid grid-cols-1 lg:grid-cols-4 gap-x-6 gap-y-6">
                     {dataEachDay.length > 0 ? dataEachDay : '...Loading weather data'}
                 </div>
